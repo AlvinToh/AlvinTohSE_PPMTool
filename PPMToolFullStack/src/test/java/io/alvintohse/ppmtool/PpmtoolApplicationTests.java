933d69d7db6f3c08f79f3b9981a26762ec785fde
Alvin Toh <alvin4292@gmail.com>
2020-08-10 22:19:59 +0800

    first commit

---
+++ PPMToolFullStack/src/test/java/io/alvintohse/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alvintohse.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
