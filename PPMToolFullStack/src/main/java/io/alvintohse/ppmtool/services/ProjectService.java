882d715958a0c16640751ef8d39baab3e01a6d33
Alvin Toh <alvin4292@gmail.com>
2020-08-13 00:45:00 +0800

    Creating Custom Exceptions

--- PPMToolFullStack/src/main/java/io/alvintohse/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/alvintohse/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.alvintohse.ppmtool.services;
 
 import io.alvintohse.ppmtool.domain.Project;
+import io.alvintohse.ppmtool.exceptions.ProjectIdException;
 import io.alvintohse.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-
-        //Logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
