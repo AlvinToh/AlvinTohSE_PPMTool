fb020dab1b886dad8ef5f459dc4e45023159d668
Alvin Toh <alvin4292@gmail.com>
2020-08-12 22:38:04 +0800

    Refactor Project Controller

--- PPMToolFullStack/src/main/java/io/alvintohse/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/alvintohse/ppmtool/web/ProjectController.java
@@ -1,18 +1,15 @@
 package io.alvintohse.ppmtool.web;
 
 import io.alvintohse.ppmtool.domain.Project;
+import io.alvintohse.ppmtool.services.MapErrorValidationService;
 import io.alvintohse.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,15 +18,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapErrorValidationService mapErrorValidationService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapErrorValidationService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
